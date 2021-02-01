f44bac95433f3ff7fe386d2668c3964241a23842
magicashu <ashuujadhav123@gmail.com>
2021-02-01 15:57:15 +0530

    custom login page enabled

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,25 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+Route::get('/lol', function(){
     return view('welcome');
 });
-Route::get('/route', function () {
+Route::get('/', function () {
     return view('index');
 });
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
+Route::get('new-login',function(){
+    return view('newLogin');
+});
+Route::get('new-register',function(){
+    return view('newRegister');
+});
+Route::get('forgot',function(){
+    return view('newForgot');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
